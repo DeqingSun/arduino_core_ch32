01904516b335b7f3b328ab24880017ef515ff744
Deqing Sun <iamseer@gmail.com>
2025-01-16 11:41:33 -0500

    adjust order

--- variants/CH57x/CH573/variant_CH573.cpp
+++ variants/CH57x/CH573/variant_CH573.cpp
@@ -18,28 +18,28 @@
 
 // Digital PinName array
 const PinName digitalPin[] = {
-  PA_4,   //D0/A0    RXD3
-  PA_5,   //D1/A1    TXD3
-  PA_8,   //D2/A12   RXD1
-  PA_9,   //D3/A13   TXD1/TMR0
-  PA_10,  //D4       TMR1/X32KI
-  PA_11,  //D5       TMR2/X32KO
-  PA_12,  //D6/A2    PWM4/SCS
-  PA_13,  //D7/A3    PWM5/SCK0
-  PA_14,  //D8/A4    MOSI/TXD0_
-  PA_15,  //D9/A5    MISO/RXD0_
-  PB_0,   //D10/A8   PWM6/CTS
-  PB_4,   //D11      PWM7/RXD0
-  PB_6,   //D12/A9   PWM8/RTS
-  PB_7,   //D13      PWM9/TXD0
+  PA_4,   //D0/A0    RXD3/AIN0
+  PA_5,   //D1/A1    TXD3/AIN1
+  PA_8,   //D2/A12   RXD1/AIN12
+  PA_9,   //D3/A13   TMR0/TXD1/AIN13
+  PA_10,  //D4       X32KI/TMR1
+  PA_11,  //D5       X32KO/TMR2
+  PA_12,  //D6/A2    SCS/PWM4/AIN2
+  PA_13,  //D7/A3    SCK0/PWM5/AIN3
+  PA_14,  //D8/A4    MOSI/TXD0_/AIN4
+  PA_15,  //D9/A5    MISO/RXD0_/AIN5
+  PB_0,   //D10/A8   PWM6/CTS/AIN8
+  PB_4,   //D11      RXD0/PWM7
+  PB_6,   //D12/A9   RTS/PWM8/AIN9
+  PB_7,   //D13      TXD0/PWM9 
   PB_10,  //D14      UD-/TMR1_
   PB_11,  //D15      UD+/TMR2_
   PB_12,  //D16      SCS_/RXD1_ (5VT)
   PB_13,  //D17      SCK0_/TXD1_ (5VT)
-  PB_14,  //D18      TIO/PWM10/DSR/MOSI_ (5VT)
-  PB_15,  //D19      TCK/DTR/MISO_ (5VT)
-  PB_22,  //D20      RXD2/TMR3
-  PB_23   //D21      RST#/PWM11/TXD2/TMR0_
+  PB_14,  //D18      TIO/MOSI_/PWM10/DSR (5VT)
+  PB_15,  //D19      TCK/MISO_/DTR (5VT)
+  PB_22,  //D20      TMR3/RXD2
+  PB_23   //D21      RST#/TMR0_/TXD2/PWM11
 };
 
 // Analog (Ax) pin number array
@@ -59,5 +59,4 @@ const uint32_t analogInputPin[] = {
   2,  // A12, PA8
   3,  // A13, PA9
 };
-
 
