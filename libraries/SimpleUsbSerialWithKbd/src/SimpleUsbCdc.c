93eb5dd5f4e795c665ce06e7d45dfe4c447049a5
Deqing Sun <iamseer@gmail.com>
2026-02-06 12:22:58 -0500

    fix race hazard in keyboard as well

--- libraries/SimpleUsbSerialWithKbd/src/SimpleUsbCdc.c
+++ libraries/SimpleUsbSerialWithKbd/src/SimpleUsbCdc.c
@@ -182,27 +182,42 @@ bool USBSerial() {
   
 void USBSerial_flush(void) {
   if (!UpPoint2BusyFlag && usbWritePointer > 0) {
-#if defined(CH573)
+      uint16_t usbIntCopy;
+#if defined(CH573) || defined(CH572)
+      usbIntCopy = R8_USB_INT_EN;
+      R8_USB_INT_EN &= ~RB_UIE_TRANSFER;
       R8_UEP2_T_LEN = usbWritePointer;
+      UpPoint2BusyFlag = 1;
       R8_UEP2_CTRL = R8_UEP2_CTRL & ~MASK_UEP_T_RES | UEP_T_RES_ACK; // Respond ACK
+      R8_USB_INT_EN = usbIntCopy;
 #elif defined(CH32X035)
+      usbIntCopy = USBFSD->INT_EN;
+      USBFSD->INT_EN &= ~USBFS_UIE_TRANSFER;
       USBFSD->UEP2_TX_LEN = usbWritePointer;
+      UpPoint2BusyFlag = 1;
       USBFSD->UEP2_CTRL_H = USBFSD->UEP2_CTRL_H & ~USBFS_UEP_T_RES_MASK | USBFS_UEP_T_RES_ACK; // Respond ACK
+      USBFSD->INT_EN = usbIntCopy;
 #endif
-    UpPoint2BusyFlag = 1;
 
     if (usbWritePointer ==
         MAX_PACKET_SIZE) { // write empty packet for end transmission. Needed
                             // for windows.
       if (USBSerial_wait_UpPoint2BusyFlag_clear()) {
-#if defined(CH573)
+#if defined(CH573) || defined(CH572)
+        usbIntCopy = R8_USB_INT_EN;
+        R8_USB_INT_EN &= ~RB_UIE_TRANSFER;
         R8_UEP2_T_LEN = 0;
-        R8_UEP2_CTRL = R8_UEP2_CTRL & ~MASK_UEP_T_RES | UEP_T_RES_ACK; // Respond ACK
-#elif defined(CH32X035)
-        USBFSD->UEP2_TX_LEN = 0;
-        USBFSD->UEP2_CTRL_H = USBFSD->UEP2_CTRL_H & ~USBFS_UEP_T_RES_MASK | USBFS_UEP_T_RES_ACK; // Respond ACK
-#endif
         UpPoint2BusyFlag = 1;
+        R8_UEP2_CTRL = R8_UEP2_CTRL & ~MASK_UEP_T_RES | UEP_T_RES_ACK; // Respond ACK
+        R8_USB_INT_EN = usbIntCopy;
+#elif defined(CH32X035)
+        usbIntCopy = USBFSD->INT_EN;
+        USBFSD->INT_EN &= ~USBFS_UIE_TRANSFER;
+        USBFSD->UEP2_TX_LEN = 0;
+        UpPoint2BusyFlag = 1;
+        USBFSD->UEP2_CTRL_H = USBFSD->UEP2_CTRL_H & ~USBFS_UEP_T_RES_MASK | USBFS_UEP_T_RES_ACK; // Respond ACK
+        USBFSD->INT_EN = usbIntCopy;
+#endif
       }
     }
     usbWritePointer = 0;
